59600d2921c71082a1839d23acd6f1de8e5f951f
littlekanine <elia.kopff3891@gmail.com>
2024-07-03 18:20:16 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,56 @@
+# variable 
+
+variable = "Bonjour le monde !"
+
+# snake_case = maniére d'ecrire en python /= camelCase des autres langages
+
+nbr_of_wheels = 4 # nombre de roues (test comm. droite)
+
+# print(variable, nbr_of_wheels)
+
+#int = nombre entier 
+a = 1 
+
+#float = nombre decimal 
+b = 2.5
+
+#str = string 
+c = "hello world !"
+
+#bool = vrai/faux 
+d = True
+
+#list = liste --> []
+e = [1, 'hello', True]
+
+#tuple = n'est pas modifiable à partir du moment qu'il est assigné --> ()
+f = (2, "hello moon", False)
+
+#set = une liste mais on ne peut pas avoir de doublon --> {}
+g = {3,"good night", -2.2}
+
+#dict association clé valeur --> {}
+h = {
+    'bonjour' : 'world',
+    3 : True,
+    True : '4'
+}
+
+#none
+i = None
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+# print(type(i))
+
+a = 1
+b = a + 2
+b +=4  # --> valeur de b + 4 = nouvelle valeur de b 
+
+print(b) # 3 of course / c'est des tests petit coquin
